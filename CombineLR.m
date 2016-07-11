93d53b58a657d98d6d424c8de38b01d2afeff6c4
Wesley Ranger <metorm@outlook.com>
2016-07-11 08:59:15 +0800

    Better CombineLR

--- CombineLR.m
+++ CombineLR.m
@@ -1,38 +1,42 @@
-function [ D ] = CombineLR( L,R,F )
+function [ D ] = CombineLR( derivL,derivR,S )
 %Gudunov's method to choose one from backward/forward differentiation
-%   L: matrix of backward differentiation
-%   R: matrix of forward differentiation
-%   F: source scalar field
+%   derivL: matrix of backward differentiation
+%   derivR: matrix of forward differentiation
+%   S: source scalar field
 %   D: return value
 
-if(sum(abs(size(F)-size(L))) + sum(abs(size(F)-size(R))) > 0)
+if(sum(abs(size(S)-size(derivL))) + sum(abs(size(S)-size(derivL))) > 0)
     error('Size of input arrays are different!')
 end
 
-S = (F>0)*2-1;
-
-SR = S.*R;
-SL = S.*L;
-
-% For Gudunov's method, check characteristic directions according to left and right derivative approximations.
-% Both directions agree that flow is to the left.
-flowL = ((SR <= 0) & (SL <= 0));
+flowL = ((S .* derivR <= 0) & (S .* derivL <= 0));
 
 % Both directions agree that flow is to the right.
-flowR = ((SR >= 0) & (SL >= 0));
+flowR = ((S .* derivR >= 0) & (S .* derivL >= 0));
 
-%Converging flow, need to check which direction arrives first.
-flows = ((SR < 0) & (SL >  0));
+% Diverging flow; entropy condition requires choosing deriv = 0
+%   (so we don't actually have to calculate this term).
+%flow0 = ((S .* derivR >  0) & (S .* derivL <  0));
 
-for i = 1:numel(flows)
-    if (flows(i))
-        s = S(i) * (abs(R(i)) - abs(L(i))) / (R(i) - L(i));
-        % If s == 0, both directions arrive at the same time. Assuming continuity, both will produce same result, so pick one.
-        flowL(i) = flowL(i) | (s < 0);
-        flowR(i) = flowR(i) | (s >= 0);
-    end
+% Converging flow, need to check which direction arrives first.
+flows = ((S .* derivR <  0) & (S .* derivL >  0));
+if(any(flows(:)))
+  conv = find(flows);
+  s = zeros(size(flows));
+  s(conv) = S(conv) .* (abs(derivR(conv)) - abs(derivL(conv))) ...
+            ./ (derivR(conv) - derivL(conv));
+
+  % If s == 0, both directions arrive at the same time.
+  %   Assuming continuity, both will produce same result, so pick one.
+  flowL(conv) = flowL(conv) | (s(conv) < 0);
+  flowR(conv) = flowR(conv) | (s(conv) >= 0);
 end
 
-D = L.*flowR + R.*flowL;
+D = derivL .* flowR + derivR .* flowL;
+
+if(sum(sum(isnan(D)))>0)
+    % if this error is triggered, set break point here to debug
+    error('NaN found in array. Something wrong.');
+end
 
 end
